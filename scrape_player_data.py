ebcd1a1afdb29564ccef3fc108dde3fff059616f
unknown <104767715+SpicyBolas@users.noreply.github.com>
2024-03-14 23:03:02 +1100

    Initial Commit

---
+++ scrape_player_data.py
@@ -0,0 +1,10 @@
+#Description: Extracts and saves player data (2012-2024) from AFL website
+#################
+#PACKAGE IMPORTS#
+from bs4 import BeautifulSoup 
+import requests
+import pandas as pd
+
+#########################
+#Variables and Constants#
+
